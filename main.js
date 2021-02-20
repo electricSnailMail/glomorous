ecc4c083dc547a4ae9c8b871c5f7122820644373
Kyle Lorelli <kyle.lorelli@gmail.com>
2021-02-20 18:39:02 -0500

    Process function adds prefs and suffs again. Added count func

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
 let upload = {
   element: document.getElementById('upload'),
-  processed: []
 }
 
 upload.process = function(wire) {
   let wordlist = wire.split('\n')
-  
+
   for(let word of wordlist) {
     word = word.trim()
     if (!word) { continue }
-    this.processed.push(word)
+
+    if (word.endsWith('-')) {
+      glomster.prefixes.push(word.slice(0, -1))
+    } else if (word.startsWith('-')) {
+      glomster.suffixes.push(word.slice(1))
+    } else {
+      glomster.roots.push(word)
+    }
   }
+
+  glomster.count()
 }
 
 upload.handleFile = function() {
@@ -29,4 +37,9 @@ let glomster = {
   suffixes:[],
   headsize: 0,
   tailsize: 0
+}
+
+glomster.count = function() {
+  this.headsize = this.prefixes.length + this.roots.length
+  this.tailsize = this.suffixes.length + this.roots.length
 }
